99c2e0449d9bb5696c8aecdea845f25063d2bc9b
Furkan Ayvaz <47609350+ayvazfurkan@users.noreply.github.com>
2023-09-01 19:38:31 +0300

    api baseUrl update

--- client/axios.config.js
+++ client/axios.config.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://app-nation.onrender.com';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
 module.exports = axios;
